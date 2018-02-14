29780c6ddf7529a10be0406c6f38013a3604d4e8
caiodsc <caio.dscamara@gmail.com>
2018-02-14 12:52:37 -0400

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'chats/show'
+  root 'chats#show'
+  mount ActionCable.server => '/cable'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
